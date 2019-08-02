d5b710d8a18bdcbce129a6d91aa2fd74451f731a
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-08-02 14:28:54 +0200

    Added setup

--- pm4pyclustering/__init__.py
+++ pm4pyclustering/__init__.py
@@ -1,1 +1,8 @@
 from pm4pyclustering import algo, util
+
+__version__ = '0.0.1'
+__doc__ = "Process Mining for Python - Clustering Support"
+__author__ = 'PADS'
+__author_email__ = 'pm4py@pads.rwth-aachen.de'
+__maintainer__ = 'PADS'
+__maintainer_email__ = "pm4py@pads.rwth-aachen.de"
---
+++ setup.py
@@ -0,0 +1,36 @@
+from os.path import dirname, join
+
+from setuptools import setup
+
+import pm4pyclustering
+
+
+def read_file(filename):
+    with open(join(dirname(__file__), filename)) as f:
+        return f.read()
+
+
+setup(
+    name=pm4pyclustering.__name__,
+    version=pm4pyclustering.__version__,
+    description=pm4pyclustering.__doc__.strip(),
+    long_description=read_file('README.md'),
+    author=pm4pyclustering.__author__,
+    author_email=pm4pyclustering.__author_email__,
+    py_modules=[pm4pyclustering.__name__],
+    include_package_data=True,
+    packages=['pm4pycvxopt', 'pm4pycvxopt.util', 'pm4pycvxopt.util.lp', 'pm4pycvxopt.util.lp.versions'],
+    url='http://www.pm4py.org',
+    license='GPL 3.0',
+    packages=['pm4pyclustering', 'pm4pyclustering.algo', 'pm4pyclustering.algo.other',
+              'pm4pyclustering.algo.other.anchors', 'pm4pyclustering.algo.other.anchors.versions',
+              'pm4pyclustering.algo.other.anchors.interface', 'pm4pyclustering.algo.other.clustering',
+              'pm4pyclustering.algo.other.clustering.versions', 'pm4pyclustering.algo.other.conceptdrift',
+              'pm4pyclustering.algo.other.conceptdrift.utils', 'pm4pyclustering.algo.other.conceptdrift.versions',
+              'pm4pyclustering.util', 'pm4pyclustering.util.anchors'],
+    project_urls={
+        'Documentation': 'http://pm4py.pads.rwth-aachen.de/documentation/',
+        'Source': 'https://github.com/pm4py/pm4py-source',
+        'Tracker': 'https://github.com/pm4py/pm4py-source/issues',
+    }
+)
