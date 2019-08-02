be6ca276474dc869ef1758b211960d3d02ecd7d8
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-08-02 14:30:35 +0200

    Fixed setup

--- setup.py
+++ setup.py
@@ -19,15 +19,18 @@ setup(
     author_email=pm4pyclustering.__author_email__,
     py_modules=[pm4pyclustering.__name__],
     include_package_data=True,
-    packages=['pm4pycvxopt', 'pm4pycvxopt.util', 'pm4pycvxopt.util.lp', 'pm4pycvxopt.util.lp.versions'],
-    url='http://www.pm4py.org',
-    license='GPL 3.0',
     packages=['pm4pyclustering', 'pm4pyclustering.algo', 'pm4pyclustering.algo.other',
               'pm4pyclustering.algo.other.anchors', 'pm4pyclustering.algo.other.anchors.versions',
               'pm4pyclustering.algo.other.anchors.interface', 'pm4pyclustering.algo.other.clustering',
               'pm4pyclustering.algo.other.clustering.versions', 'pm4pyclustering.algo.other.conceptdrift',
               'pm4pyclustering.algo.other.conceptdrift.utils', 'pm4pyclustering.algo.other.conceptdrift.versions',
-              'pm4pyclustering.util', 'pm4pyclustering.util.anchors'],
+              'pm4pyclustering.util', 'pm4pyclustering.util.anchors'],    url='http://www.pm4py.org',
+    license='GPL 3.0',
+    install_requires=[
+        'pm4py',
+        'joblib',
+        'lime'
+    ],
     project_urls={
         'Documentation': 'http://pm4py.pads.rwth-aachen.de/documentation/',
         'Source': 'https://github.com/pm4py/pm4py-source',
